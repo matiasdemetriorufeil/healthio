14ef972fa439a624fe927dbcaa5ba1a364e806bf
DemetrioRufeil <demetriorufeil@gmail.com>
2026-06-28 15:37:04 -0300

    ajustes toolbar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'healthio-v20';
+const CACHE = 'healthio-v21';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
