62de4d0e40d81e3b7f1f67b2eebdb8744c804f56
DemetrioRufeil <demetriorufeil@gmail.com>
2026-06-28 14:52:03 -0300

    ajustes viewport

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'healthio-v17';
+const CACHE = 'healthio-v18';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
